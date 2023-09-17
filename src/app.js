126abf1db20bf7ffd777f3151e96be4137d81e10
sescandon <sescandon@unal.edu.co>
2023-09-17 14:31:48 -0500

    Added CORS

--- src/app.js
+++ src/app.js
@@ -3,9 +3,13 @@ import morgan from "morgan";
 import authRoutes from "./routes/auth.routes.js";
 import taskRoutes from "./routes/task.routes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+}));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
