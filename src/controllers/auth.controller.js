d76e8df245068963914fa5f2889e108c929e70c1
sescandon <sescandon@unal.edu.co>
2023-09-21 23:12:46 -0500

    Cors policy updated and new route for token validation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,15 +1,15 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { createAccessToken } from "../libs/jwt.js";
-import { authRequired } from "../middlewares/validateToken.js";
+import jwt from "jsonwebtoken";
+import { TOKEN_SECRET } from "../config.js";
 
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
 
   try {
-
-    const userFound = await User.findOne({email})
-    if(userFound) return res.status(400).json(["Email already in use"])
+    const userFound = await User.findOne({ email });
+    if (userFound) return res.status(400).json(["Email already in use"]);
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({
@@ -70,7 +70,7 @@ export const logout = (req, res) => {
   return res.sendStatus(200);
 };
 
-export const profile = async(req, res) => {
+export const profile = async (req, res) => {
   const userFound = await User.findById(req.user.id);
 
   if (!userFound) return res.status(400).json({ message: "User not found" });
@@ -81,5 +81,24 @@ export const profile = async(req, res) => {
     email: userFound.email,
     createdAt: userFound.createdAt,
     updatedAt: userFound.updatedAt,
+  });
+};
+
+export const verifyToken = async (req, res) => {
+  const { token } = req.cookies;
+
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+  jwt.verify(token, TOKEN_SECRET, async (err, user) => {
+    if (err) return res.status(401).json({ message: "Unauthorized" });
+
+    const userFound = await User.findById(user.id);
+    if (!userFound) return res.status(401).json({ message: "Unauthorized" });
+
+    return res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email
+    })
   });
 };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,6 +4,7 @@ import {
   register,
   logout,
   profile,
+  verifyToken,
 } from "../controllers/auth.controller.js";
 import { authRequired } from "../middlewares/validateToken.js";
 import { validateSchema } from "../middlewares/validateTypes.js";
@@ -11,10 +12,11 @@ import { registerSchema, loginSchema } from "../schemas/auth.schema.js";
 
 const router = Router();
 
-router.post("/register",validateSchema(registerSchema), register);
-router.post("/login",validateSchema(loginSchema) ,login);
+router.post("/register", validateSchema(registerSchema), register);
+router.post("/login", validateSchema(loginSchema), login);
 router.post("/logout", logout);
 
 router.get("/profile", authRequired, profile);
+router.get("/verify", verifyToken);
 
 export default router;
